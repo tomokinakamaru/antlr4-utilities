a99286af580b282ac23562d2f561f2f4e49f84fd
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-18 00:22:52 +0900

    Add abstract parser/listener

---
+++ src/main/java/com/github/tomokinakamaru/antlr4utilities/AbstractListener.java
@@ -0,0 +1,29 @@
+package com.github.tomokinakamaru.antlr4utilities;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+public abstract class AbstractListener extends AbstractAnalyzer implements ParseTreeListener {
+
+  protected abstract Class<? extends ParserRuleContext> getContextClass();
+
+  @Override
+  public final void analyze() {
+    ParseTreeWalker.DEFAULT.walk(this, get(getContextClass()));
+  }
+
+  @Override
+  public void visitTerminal(TerminalNode node) {}
+
+  @Override
+  public void visitErrorNode(ErrorNode node) {}
+
+  @Override
+  public void enterEveryRule(ParserRuleContext ctx) {}
+
+  @Override
+  public void exitEveryRule(ParserRuleContext ctx) {}
+}
