466e97abf1740bd9c47f7f4e50a67fb48591b047
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-18 00:00:02 +0900

    Add symbol table

---
+++ src/main/java/com/github/tomokinakamaru/antlr4utilities/AbstractSymbolTable.java
@@ -0,0 +1,55 @@
+package com.github.tomokinakamaru.antlr4utilities;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public abstract class AbstractSymbolTable<T extends AbstractSymbolTable<T, E>, E> {
+
+  protected abstract T newScope();
+
+  AbstractSymbolTable<T, E> parent = null;
+
+  private final Map<String, E> map = new LinkedHashMap<>();
+
+  public final boolean has(String key) {
+    if (map.containsKey(key)) {
+      return true;
+    }
+    if (parent == null) {
+      return false;
+    }
+    return parent.has(key);
+  }
+
+  public final E get(String key) {
+    if (map.containsKey(key)) {
+      return map.get(key);
+    }
+    if (parent == null) {
+      return defaultEntity(key);
+    }
+    return parent.get(key);
+  }
+
+  public final void set(String key, E entity) {
+    if (map.containsKey(key)) {
+      map.put(key, resolveConflict(key, map.get(key), entity));
+    } else {
+      map.put(key, entity);
+    }
+  }
+
+  public final T newChildScope() {
+    T table = newScope();
+    table.parent = this;
+    return table;
+  }
+
+  protected E resolveConflict(String key, E oldEntity, E newEntity) {
+    throw new RuntimeException();
+  }
+
+  protected E defaultEntity(String key) {
+    throw new RuntimeException();
+  }
+}
