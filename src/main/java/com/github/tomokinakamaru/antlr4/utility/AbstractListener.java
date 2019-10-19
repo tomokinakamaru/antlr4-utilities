aa6a8293a1ba4a04f8788d6be6d6db353b534792
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2019-10-19 09:41:50 +0900

    Revise listener

--- src/main/java/com/github/tomokinakamaru/antlr4/utility/AbstractListener.java
+++ src/main/java/com/github/tomokinakamaru/antlr4/utility/AbstractListener.java
@@ -1,29 +1,15 @@
 package com.github.tomokinakamaru.antlr4.utility;
 
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.ErrorNode;
-import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.antlr.v4.runtime.tree.TerminalNode;
 
-public abstract class AbstractListener extends AbstractAnalyzer implements ParseTreeListener {
+public abstract class AbstractListener extends AbstractAnalyzer
+    implements DefaultParseTreeListener {
 
-  protected abstract Class<? extends ParserRuleContext> getContextClass();
+  protected abstract ParserRuleContext getContext();
 
   @Override
   public final void analyze() {
-    ParseTreeWalker.DEFAULT.walk(this, get(getContextClass()));
+    ParseTreeWalker.DEFAULT.walk(this, getContext());
   }
-
-  @Override
-  public void visitTerminal(TerminalNode node) {}
-
-  @Override
-  public void visitErrorNode(ErrorNode node) {}
-
-  @Override
-  public void enterEveryRule(ParserRuleContext ctx) {}
-
-  @Override
-  public void exitEveryRule(ParserRuleContext ctx) {}
 }
